655d80a9bd21638e59c8ecdf752f29768c38118f
shenriquezneira <141794138+shenriquezneira@users.noreply.github.com>
2024-09-10 22:24:48 -0300

    Drilling

---
+++ assets/js/javascript.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    console.log('paso')
+    $('#btnSubmit').click(function () {
+
+        let name = $('#inputName').val()
+        let email = $('#inputEmail').val()
+        let message = $('#inputMessage').val()
+
+        if (name != '' && email != '' && message != '') {
+            $('#pName').html(`<strong>Nombre: </strong>${name}`)
+            $('#pEmail').html(`<strong>Correo: </strong>${email}`)
+            $('#pMessage').html(`<strong>Mensaje: </strong>${message}`)
+            $('#inputName').val('')
+            $('#inputEmail').val('')
+            $('#inputMessage').val('')
+        }
+        else {
+            alert('Falta Ingresar un campo')
+        }
+
+
+    })
+})
